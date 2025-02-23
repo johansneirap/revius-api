e4ce491340b9b2b38a1bace5a123cf59e8c836e9
Johansen <johansneirap@gmail.com>
2025-02-23 04:03:33 +0000

    feat(products): add endpoint to retrieve a single product by ID

--- app/routers/products.py
+++ app/routers/products.py
@@ -7,3 +7,8 @@ router = APIRouter(prefix="/products", tags=["products"])
 async def read_products():
     """read products controller"""
     return [{"product": "product 1"}, {"product": "product 2"}]
+
+@router.get("/{product_id}")
+async def read_product(product_id: int):
+    """read product controller"""
+    return {"product_id": product_id}
